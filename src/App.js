eafd79c84411af8e10e6cfd8e18cd3884132bdd3
utsavc <utsavmahato05@gmail.com>
2022-09-09 23:16:45 +0545

    Added Jokes.js, JokesData.js and made changes in App.js

--- src/App.js
+++ src/App.js
@@ -4,18 +4,32 @@ import img1 from "./images/img1.jpg"
 import img2 from "./images/img2.jpg"
 import img3 from "./images/img3.jpg"
 import img4 from "./images/img4.jpg"
+import Jokes from "./Jokes";
+import JokesData from "./JokesData";
+
+/*This returns to the Joke.js files which accepts the props setup and punchline, The array is 
+imported from the JokesData file*/
+
 function App() {
+  const jokeElements = JokesData.map(joke => {
+    return <Jokes setup={joke.setup} punchline={joke.punchline} />
+})
+
+
   let a="Lorem ipsum dolor sit amet,consectetur adipisicing elit,sed do eiusmo tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, ";
   return (
     <div className="App">
       <h2>Things to Expect</h2>
       <div className="container">
-      <Card title="Nice Bird" source={img1} content={a}/>
-      <Card title="Dusk" source={img2} content={a}/>
+      <Card title="Nice Bird" source={img1} content={a} like={420}/>
+      <Card title="Dusk" source={img2} content={a} like={677}/>
       <Card title="Autumn's Arrival " source={img3} content={a}/>
       <Card title="Elephant" source={img4} content={a}/>
+     
       </div>
-
+      <div className="joke-container">
+      {jokeElements}
+      </div>
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -7,6 +7,7 @@ export default function Card(props) {
                 <img class="card-image" src={props.source} />
                 <h2 className="card-title">{props.title}</h2>
                 <p className="card-content">{props.content}</p>
+                {props.like && <span>&hearts; {props.like}</span>}
             </div>
     )
 }
